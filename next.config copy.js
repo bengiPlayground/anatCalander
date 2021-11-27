e3c8b939f845dda74f59a87daf54c555d01573ac
bengi28 <dorbengi@gmail.com>
2021-11-27 19:57:24 +0200

    state9

--- next.config copy.js
+++ next.config copy.js
@@ -12,29 +12,6 @@ module.exports = (phase) => {
   console.log(`isDev:${isDev}  isProd:${isProd}   isStaging:${isStaging}`);
 
   const env = {
-    RESTURL_SPEAKERS: (() => {
-      if (isDev) return "http://localhost:4000/speakers";
-      if (isProd) {
-        return "https://www.siliconvalley-codecamp.com/rest/speakers/ps";
-      }
-      if (isStaging) return "http://localhost:11639";
-      return "RESTURL_SPEAKERS:not (isDev,isProd && !isStaging,isProd && isStaging)";
-    })(),
-    RESTURL_SESSIONS: (() => {
-      if (isDev) return "http://localhost:4000/sessions";
-      if (isProd) return "https://www.siliconvalley-codecamp.com/rest/sessions";
-      if (isStaging) return "http://localhost:11639";
-      return "RESTURL_SESSIONS:not (isDev,isProd && !isStaging,isProd && isStaging)";
-    })(),
-    RESTURL_SOCKETS: (() => {
-      if (isDev) return "http://localhost:4000";
-      if (isProd) return "https://h2h-tester.herokuapp.com";
-      // if (isStaging) return "http://localhost:11639";
-      return "RESTURL_SOCKETS:not (isDev,isProd && !isStaging,isProd && isStaging)";
-    })(),
-    API_BASE_URL: "https://soccer.sportmonks.com/api/v2.0",
-    API_TOKEN: "Tof53Ing3VoDALtQSoGLCYMvktGEXSyGM4UAZRbciqkpqhtAGI7vjtv68u6S",
-    // API_TOKEN: "X24XJih4JMDTP8sqABulLIhje48aTS5Xp3IUuaMnvm3iS2P5yFKzVrFvrsvo",
     MONGODB_URI:
       "mongodb+srv://admin:admin593@cluster0.stfp2.mongodb.net/anat?retryWrites=true&w=majority",
 
