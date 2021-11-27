48634e9dd7e77fb9fd35aeafc3b24a7712dbe487
bengi28 <dorbengi@gmail.com>
2021-11-27 20:06:26 +0200

    state12

--- next.config.js
+++ next.config.js
@@ -1,16 +1,4 @@
-// This uses phases as outlined here: https://nextjs.org/docs/#custom-configuration
 module.exports = (phase) => {
-  // when started in development mode `next dev` or `npm run dev` regardless of the value of STAGING environmental variable
-  const isDev = phase === PHASE_DEVELOPMENT_SERVER;
-  // when `next build` or `npm run build` is used
-  const isProd =
-    phase === PHASE_PRODUCTION_BUILD && process.env.STAGING !== "1";
-  // when `next build` or `npm run build` is used
-  const isStaging =
-    phase === PHASE_PRODUCTION_BUILD && process.env.STAGING === "1";
-
-  console.log(`isDev:${isDev}  isProd:${isProd}   isStaging:${isStaging}`);
-
   const env = {
     MONGODB_URI:
       "mongodb+srv://admin:admin593@cluster0.stfp2.mongodb.net/anat?retryWrites=true&w=majority",
